6df8531f01858d435b8d4569c9d83130a38665a2
KingA2009 <ajamoliddinov12@gmail.com>
2023-05-15 22:42:21 +0500

    fix

--- 7-day/app.js
+++ 7-day/app.js
@@ -1,15 +1,28 @@
-let countEl = document.getElementById('count')
-let saveEl = document.getElementById('save')
-let bnt1 = document.querySelector('.button1')
-let btn2 = document.querySelector('.button2')
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const buttons = document.querySelectorAll("button");
 
-let count = 0
-bnt1.addEventListener('click', () => {
-  count = count + 1
-  countEl.textContent = count
-})
+input.addEventListener(
+  "keyup",
+  () => (output.innerHTML = input.value)
+);
 
-btn2.addEventListener('click', () => {
-  count = count - 1
-  countEl.textContent = count
-})
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("uppercase")) {
+      output.innerHTML = output.innerHTML.toUpperCase();
+    } else if (btn.classList.contains("lowercase")) {
+      output.innerHTML = output.innerHTML.toLocaleLowerCase();
+    } else if (btn.classList.contains("capitalize")) {
+      output.innerHTML =
+        output.innerHTML.charAt(0).toUpperCase() +
+        output.innerHTML.slice(1).toLowerCase();
+    } else if (btn.classList.contains("bold")) {
+      output.style.fontWeight = "900";
+    } else if (btn.classList.contains("italic")) {
+      output.style.fontStyle = "italic";
+    } else if (btn.classList.contains("underline")) {
+      output.style.textDecoration = "underline";
+    }
+  });
+});
